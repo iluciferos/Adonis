4260d621fa56ec35b95e2fc3e6425db4f721d058
iluciferrrrrrrrrr <foerot43@gmail.com>
2023-09-18 11:09:56 +0700

    update auth

--- start/routes.js
+++ start/routes.js
@@ -30,3 +30,21 @@ Route.post('/posts/update/:id', 'PostController.update').as('posts.update')
 
 // Delete data
 Route.get('/posts/delete/:id', 'PostController.delete').as('posts.delete')
+
+/**
+* register
+*/
+Route.get('register', 'Auth/RegisterController.index').as('register.index').middleware(['RedirectIfAuthenticated'])
+Route.post('register', 'Auth/RegisterController.store').as('register.store').middleware(['RedirectIfAuthenticated'])
+
+/**
+* login
+*/
+Route.get('login', 'Auth/LoginController.index').as('login.index').middleware(['RedirectIfAuthenticated'])
+Route.post('login', 'Auth/LoginController.check').as('login.check').middleware(['RedirectIfAuthenticated'])
+Route.get('logout', 'Auth/LoginController.logout').as('logout').middleware(['Authenticate'])
+
+/**
+* dashboard
+*/
+Route.get('dashboard', 'DashboardController.index').as('dashboard').middleware(['Authenticate'])
