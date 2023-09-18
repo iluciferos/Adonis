bbdaff3cf31c42c968409001f0434bccffea15ef
Victor Kurniawan <124770602+iluciferos@users.noreply.github.com>
2023-09-18 14:30:38 +0700

    mentah

---
+++ database/migrations/1695011269016_pegawais.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'pegawais'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id');
+      table.string('nama');
+      table.enu('gender', ["L", "P"]);
+      table.integer("tinggi_badan");
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
